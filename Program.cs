d1c30a95f26d12eccec127f0af5fe86d7604f330
falau <falau@LENOVO>
2024-10-29 20:36:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a= ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b=");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Valori initaile: a={a} b={b}");
+
+            (a,b)=(b,a);
+            Console.WriteLine($"Valori dupa swap: a={a} b={b}");
+        }
+    }
+}
